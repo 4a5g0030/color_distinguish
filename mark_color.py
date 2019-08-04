a4b2c063fc90d587f4eb3fd5aa34670db0275588
ZW_Li <4a5g0030@stust.edu.tw>
2019-08-05 05:58:12 +0800

    matk_ok!!

--- mark_color.py
+++ mark_color.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 import color_distinguish as cd
 
-path = 'test_data\RGB.png'
+c_th = 2000
+path = 'test_data/test_myroom.jpg'
 image = cv2.imread(path)
 mark_img = cd.color_distinguish(image)
 
@@ -15,10 +16,46 @@ green = mark_img.res_green()
 kernal = np.ones((5, 5), 'uint8')
 
 red = cv2.dilate(red, kernal)
-res = cv2.bitwise_and(image, image, mask = red)
+res = cv2.bitwise_and(image, image, mask=red)
 
 blue = cv2.dilate(blue, kernal)
-res1 = cv2.bitwise_and(image, image, mask = blue)
+res1 = cv2.bitwise_and(image, image, mask=blue)
 
 green = cv2.dilate(green, kernal)
-res2 = cv2.bitwise_and(imshow, imshow, mask = green)
+res2 = cv2.bitwise_and(image, image, mask=green)
+
+contours, hierarchy = cv2.findContours(
+    red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area > c_th):
+        x, y, w, h = cv2.boundingRect(contour)
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(image, "RED color", (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+
+contours, hierarchy = cv2.findContours(
+    green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area > c_th):
+        x, y, w, h = cv2.boundingRect(contour)
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(image, "GREEN color", (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
+
+contours, hierarchy = cv2.findContours(
+    blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area > c_th):
+        x, y, w, h = cv2.boundingRect(contour)
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(image, "BLUE color", (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0))
+
+cv2.imshow('image', image)
+cv2.waitKey(0)
