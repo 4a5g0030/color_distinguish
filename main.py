1351d65ccb106beca2e7c68f8cc357ce66b49edf
Lizh <4a5g0030@stust.edu.tw>
2019-12-25 11:25:35 +0800

    for 12/25 demo

--- main.py
+++ main.py
@@ -7,14 +7,29 @@ def main():
     cap = cv2.VideoCapture(0)
     cap.set(3, 1280)
     cap.set(4, 720)
+    color = 0
 
     while True:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
         frame = cv2.GaussianBlur(frame, (5, 5), 1.5)
-        frame, color_box = mc.mark_color(frame, 108000)
+        frame, color_box = mc.mark_color(frame, 72000)
+
+        if color is 0:
+            if color_box[1] is not 0:
+                color = 1
+            if color_box[2] is not 0:
+                color = 2
+
         cv2.imshow('frame', frame)
-        p = 'R : ' + str(color_box[0]) + ', G : ' + str(color_box[1]) + ', B : ' + str(color_box[2])
+        p = 'R : ' + str(color_box[0]) + ', G : ' + str(color_box[1]) + ', B : ' + str(
+            color_box[2]) + " , Lock Color :" + str(color)
+
+        if color_box[color] is not 0:
+            p += " ON"
+        elif color_box[0] is not 0:
+            p += " OFF"
+
         sys.stdout.write('\r' + p)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -24,7 +39,7 @@ def main():
 
 def t_one():
     img = cv2.imread('test_data/RGB.png')
-    img = mc.mark_color(img, 300)
+    img = mc.mark_color(img, 30)
     cv2.imshow('img', img)
     cv2.waitKey(0)
 
